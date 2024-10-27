a9a5471c1c33e234c275192a4e1eb6fbbf95b887
Mareks "NotS1ick" Lovniks <69733624+NotS1ick@users.noreply.github.com>
2024-10-27 04:22:58 +0200

    Update LoginVM.cs

--- Havit/ViewModels/LoginVM.cs
+++ Havit/ViewModels/LoginVM.cs
@@ -1,16 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
-namespace Havit.ViewModels;
-
-public class LoginVM
+namespace Havit.ViewModels
 {
-    [Required(ErrorMessage = "Username is required")]
-    public string Username { get; set; }
-    
-    [Required(ErrorMessage = "Password is required")]
-    [DataType(DataType.Password)]
-    public string Password { get; set; }
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; } = string.Empty;
 
-    [Display(Name = "Remember me?")] 
-    public bool RememberMe { get; set; } = false;
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; } = false;
+    }
 }
