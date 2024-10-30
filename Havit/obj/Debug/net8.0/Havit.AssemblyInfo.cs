1dfa8dd9e2704a32ff8f5f93925a48eedda74803
Mareks "NotS1ick" Lovniks <69733624+NotS1ick@users.noreply.github.com>
2024-10-30 23:21:45 +0200

    FIrst commit

--- Havit/obj/Debug/net8.0/Havit.AssemblyInfo.cs
+++ Havit/obj/Debug/net8.0/Havit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Havit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006d50a0bb602dbbafa1f0eee1abe7cbba725827")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a4272ce375371922e6493749dff66879a209d0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Havit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Havit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
